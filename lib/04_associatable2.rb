2c9130cc6d36e5f2c19ff5d6cfd2a6fdda533381
agarun <19801205+agarun@users.noreply.github.com>
2017-11-24 16:58:22 -0500

    Implement `has_many_through`

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,58 @@
 require_relative '03_associatable'
 
-# Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
-
+  # handle options inside so that inflector will constantize without error
+  # inside `define_method`, `self` is the *instance*, outside it's the *class*
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      source_table = source_options.table_name
+
+      through_p_k = through_options.primary_key
+      source_p_k = source_options.primary_key
+
+      through_f_k = through_options.foreign_key
+      source_f_k = source_options.foreign_key
+
+      record_hashes = DBConnection.execute(<<-SQL, self.owner_id)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_f_k} = #{source_table}.#{through_p_k}
+        WHERE
+          #{through_table}.#{through_p_k} = ?
+      SQL
+
+      source_options
+        .model_class
+        .parse_all(record_hashes)
+        .first
+    end
   end
 end
+
+# as an example, we are mimicking:
+#
+# class Cat < ApplicationRecord
+#   has_one :home,
+#     through: :human,
+#     source: :house
+# end
+#
+# ..trying to accomplish the following query in the cat model:
+#
+# SELECT
+#   houses.*
+# FROM
+#   humans
+# JOIN
+#   houses ON humans.house_id = houses.id
+# WHERE
+#   humans.id = ?
